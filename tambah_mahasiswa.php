a08135d4ed2e4ad3ebfc6caafbaa9ad9aa291a41
phii09 <selfianiphiw@gmail.com>
2024-11-24 10:36:57 +0700

    Add files via upload

---
+++ tambah_mahasiswa.php
@@ -0,0 +1,32 @@
+<DOCYTPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Tambah Data Mahasiswa</title>
+</head>
+
+<body>
+    <h2>Tambah Data Mahasiswa</title>
+    <form action="proses_Tambah.php" methad="POST">
+        <label>Nama:</label>
+        <input type="text" name="nama" required><br>
+
+        <label>NIM:</label>
+        <input type="text" name="nim" required><br>
+
+
+        <label>Email:</label>
+        <input type="email" name="email" required><br>
+
+        <label>Nomor:</label>
+        <input type="text" name="jurusan" required><br>
+
+        <input type="submit" value="Tambah">
+    </form>
+</body>
+
+
+<html>
+    
+
